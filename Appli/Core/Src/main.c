2d3b2fd9af00fa7f73ac9a20ad77bb787adaef81
leon.angele <leon.angele@burger-engineering.de>
2026-05-15 21:42:40 +0200

    Fixed RAm Adresses

--- Appli/Core/Src/main.c
+++ Appli/Core/Src/main.c
@@ -445,7 +445,6 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
